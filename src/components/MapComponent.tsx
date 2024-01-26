b66e8d571470bbab4029c0f08892293acc6a6523
Tommy Yang <tommy_yang621@hotmail.com>
2024-01-26 13:02:22 -0500

    change the location to nola salon

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,16 +1,12 @@
 import React from "react";
+
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string;
 function MapComponent() {
-  const position = { lat: 61.2176, lng: -149.8997 }; // Example coordinates (Anchorage, AK)
-  console.log(apiKey);
+  const position = { lat: 43.795719146728516, lng: -79.43389892578125 }; // Nola Salon, Thornhill, ON
   return (
     <APIProvider apiKey={apiKey}>
-      <Map
-        center={position}
-        zoom={10}
-        style={{ width: "40%", height: "400px" }}
-      >
+      <Map center={position} zoom={5} style={{ width: "40%", height: "400px" }}>
         <Marker position={position} />
       </Map>
     </APIProvider>
